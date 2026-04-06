ab2434f43de9824e2c3dca602031f4753bef48ac
Taznoor Pasha <taznur27@gmail.com>
2026-04-06 10:01:46 +0530

    update app changes

--- vite.config.ts
+++ vite.config.ts
@@ -15,18 +15,3 @@ export default defineConfig({
     }
   }
 })
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
-
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  },
-  build: {
-    chunkSizeWarningLimit: 2000
-  }
-})
